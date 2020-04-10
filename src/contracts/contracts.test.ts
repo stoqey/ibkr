65f3d6d5f4737e6d939de80a6ad4b246688f7138
Ceddy Muhoza <ceddymuhoza@gmail.com>
2020-04-10 14:50:02 -0400

    Get contract details

--- src/contracts/contracts.test.ts
+++ src/contracts/contracts.test.ts
@@ -1,8 +1,4 @@
-// {"conId":347408543,"symbol":"MAXR","secType":"STK","expiry":"","strike":0,"right":"","multiplier":"","exchange":"NYSE","currency":"USD","localSymbol":"MAXR","tradingClass":"MAXR"}
-
 import 'mocha';
-import { expect } from 'chai';
-import isEmpty from 'lodash/isEmpty';
 import { getContractDetails } from './ContractDetails';
 
 const symbol = "ITCI"
@@ -10,10 +6,16 @@ const symbol = "ITCI"
 // const contractDetails = { "conId": 141253540, "symbol": "ITCI", "secType": "STK", "expiry": "", "strike": 0, "right": "", "multiplier": "", "exchange": "NASDAQ", "currency": "USD", "localSymbol": "ITCI", "tradingClass": "NMS" }
 
 describe('Contracts', () => {
-    it('should get contract details', async t => {
-        const accountSummary = await getContractDetails(symbol);
-        const currenySymbol = accountSummary && accountSummary.summary && accountSummary.summary.symbol;
-        console.log('Contract details', currenySymbol);
-        expect(currenySymbol).to.be.equals(symbol);
+    it('should get contract details', (done) => {
+        async function getContractDetailsData() {
+            const accountSummary = await getContractDetails(symbol);
+            const currenySymbol = accountSummary && accountSummary.summary && accountSummary.summary.symbol;
+            if (currenySymbol === symbol) {
+                return done();
+            }
+
+            done(new Error('Error getting symbol details'));
+        }
+        getContractDetailsData();
     });
 })
