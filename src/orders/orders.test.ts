47ebd3643baa962b8ba7be0883b4e1bbdd85c42f
Ceddy Muhoza <ceddymuhoza@gmail.com>
2020-05-09 10:52:14 -0400

    add order tests

--- src/orders/orders.test.ts
+++ src/orders/orders.test.ts
@@ -1,3 +1,69 @@
-// Make order
-// Get order
-// Delete order
+import 'mocha';
+import { expect } from 'chai';
+import { OrderTrade } from './OrderTrade';
+import { onConnected } from '../connection/connection.utilities';
+import { OrderStock } from './orders.interfaces';
+import { IbkrEvents, IBKREVENTS } from '../events';
+
+const ibkrEvents = IbkrEvents.Instance;
+
+const fsPromises = require('fs').promises
+
+const orderTrade = OrderTrade.Instance;
+let demoSymbolData;
+
+const symbol = "PECK";
+const order = [2,]
+
+const stockOrderBuyIn: OrderStock = {
+    symbol: symbol,
+    action: "BUY",
+    type: "market",
+    parameters: [2, "9999"], // 'SELL', 1, 9999,
+    size: 3,
+    capital: 1000,
+    exitTrade: false
+}
+
+const stockOrderBuyOut: OrderStock = {
+    symbol: symbol,
+    action: "SELL",
+    type: "market",
+    parameters: ["1", "9999"], // 'SELL', 1, 9999,
+    size: 3,
+    capital: 1000,
+    exitTrade: true,
+    exitParams: {
+        entryTime: new Date(),
+        entryPrice: 0,
+        exitTime: new Date(),
+        exitPrice: 0
+    }
+}
+
+describe('Orders', () => {
+
+    it('Place Order', async () => {
+
+        let results = null;
+
+        const getPlacedOrder = () => new Promise((resolve, reject) => {
+            const handleData = (data) => {
+                ibkrEvents.off(IBKREVENTS.SAVE_ORDER, handleData);
+                resolve(data)
+            };
+            ibkrEvents.on(IBKREVENTS.SAVE_ORDER, handleData);
+        });
+
+        if (await onConnected()) {
+
+            await orderTrade.placeOrder(stockOrderBuyIn);
+            results = await getPlacedOrder();
+
+        }
+        expect(results).to.be.not.null;
+
+    });
+})
+
+
