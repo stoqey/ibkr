67cb36ef8e0335cb15f150f093b6129e2b575f8e
Ceddy Muhoza <ceddymuhoza@gmail.com>
2020-06-24 03:14:45 -0400

    fix eslint

--- src/dev.ts
+++ src/dev.ts
@@ -1,12 +1,10 @@
 import 'mocha';
-import { expect } from 'chai';
+import {expect} from 'chai';
 import ibkr from '.';
 
 const main = async () => {
     const connectionStatus = await ibkr();
     expect(connectionStatus).to.be.equal(true);
 };
-
-
 
 main();
