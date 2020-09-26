4bf7ebd4904c18ae05883335002c6e280df0b7dc
Ceddy Muhoza <ceddymuhoza@gmail.com>
2020-09-26 18:37:38 -0400

    forex mk

--- src/contracts/contracts.test.ts
+++ src/contracts/contracts.test.ts
@@ -2,6 +2,7 @@ import 'mocha';
 import { getContractDetails } from './ContractDetails';
 import ibkr from '..';
 import { log } from '../log';
+import { IBKRConnection } from '../connection';
 
 const symbol = "ITCI"
 // const contractDetails = { "conId": 347408543, "symbol": "MAXR", "secType": "STK", "expiry": "", "strike": 0, "right": "", "multiplier": "", "exchange": "NYSE", "currency": "USD", "localSymbol": "MAXR", "tradingClass": "MAXR" }
@@ -12,30 +13,49 @@ before((done) => {
 });
 
 describe('Contracts', () => {
-    it('should get contract details by symbol', (done) => {
-        async function getContractDetailsData() {
-            const contractDetails = await getContractDetails(symbol);
-            if (!Array.isArray(contractDetails)) {
-                log('contract ', JSON.stringify(contractDetails))
-                return done();
-            }
-            done(new Error('Error getting symbol details'));
-        }
-        getContractDetailsData();
-    });
+    // it('should get contract details by symbol', (done) => {
+    //     async function getContractDetailsData() {
+    //         const contractDetails = await getContractDetails(symbol);
+    //         if (!Array.isArray(contractDetails)) {
+    //             log('contract ', JSON.stringify(contractDetails))
+    //             return done();
+    //         }
+    //         done(new Error('Error getting symbol details'));
+    //     }
+    //     getContractDetailsData();
+    // });
 
     it('should get contract details by contract object', (done) => {
         async function getContractDetailsData() {
-            const contractDetails = await getContractDetails({
+            const ib = IBKRConnection.Instance.getIBKR();
+
+            // @ts-ignore
+            // const contractOb = ib.contract.option('AAPL', '202012', 500, 'C');
+
+            const contractObj = {
+                // "conId":12087792,
                 currency: 'USD',
                 exchange: 'SMART',
-                //expiry: '202007',
+                multiplier: 100,
+                right: 'C',
+                secType: 'OPT',
+                // strike: 300,
+                symbol: 'AAPL'
+            }
+
+            const contractDetails = await getContractDetails(contractObj);
+
+            /**
+             {
+                currency: 'USD',
+                exchange: 'SMART',
                 multiplier: 100,
                 right: 'C',
                 secType: 'OPT',
                 strike: 300,
                 symbol: 'AAPL'
-            });
+            }
+             */
             if (contractDetails) {
                 log('contract ', JSON.stringify(contractDetails))
                 return done();
--- src/realtime/realtime.test.ts
+++ src/realtime/realtime.test.ts
@@ -5,11 +5,10 @@ import { onConnected } from '../connection/connection.utilities';
 import { IbkrEvents, IBKREVENTS } from '../events';
 import ibkr from '..';
 import { IBKRConnection } from '../connection';
+import { ContractObject } from '../contracts';
 
 const ibkrEvents = IbkrEvents.Instance;
 
-
-let symbol = 'USD.EUR';
 
 before((done) => {
     ibkr().then(r => done())
@@ -21,6 +20,8 @@ describe('Realtime', () => {
 
     // it('should get price updates for AAPL', (done) => {
 
+    //     const symbol = 'AAPL';
+
     //     const getMarketData = () => {
     //         const handleData = (data) => {
     //             // ibkrEvents.off(IBKREVENTS.ON_PRICE_UPDATES, handleData);
@@ -28,7 +29,7 @@ describe('Realtime', () => {
     //         };
     //         ibkrEvents.on(IBKREVENTS.ON_PRICE_UPDATES, handleData);
 
-    //         ibkrEvents.emit(IBKREVENTS.SUBSCRIBE_PRICE_UPDATES, { symbol });
+    //         ibkrEvents.emit(IBKREVENTS.SUBSCRIBE_PRICE_UPDATES, { contract: symbol, opt: { tickType: 'ASK'} });
 
     //         return setTimeout(() => {
     //             ibkrEvents.off(IBKREVENTS.ON_PRICE_UPDATES, handleData);
@@ -41,9 +42,12 @@ describe('Realtime', () => {
 
     it('should get price updates for forex', (done) => {
 
-        symbol = 'USD.EUR';
         const ib = IBKRConnection.Instance.getIBKR();
-        const contract = ib.contract.forex('CAD');
+
+        // @ts-ignore
+        const contract = ib.contract.forex('GBP.USD') as any
+        
+        // const ib = IBKRConnection.Instance.getIBKR();
 
         const getMarketData = () => {
             const handleData = (data) => {
@@ -52,9 +56,7 @@ describe('Realtime', () => {
             };
             ibkrEvents.on(IBKREVENTS.ON_PRICE_UPDATES, handleData);
 
-            
-
-            ibkrEvents.emit(IBKREVENTS.SUBSCRIBE_PRICE_UPDATES, contract);
+            ibkrEvents.emit(IBKREVENTS.SUBSCRIBE_PRICE_UPDATES, {contract, opt: { tickType: 'ASK' }});
 
             return setTimeout(() => {
                 ibkrEvents.off(IBKREVENTS.ON_PRICE_UPDATES, handleData);
