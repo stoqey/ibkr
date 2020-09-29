ef48408c69b61ed68edf4f6e163f7a99215d4f37
Ceddy Muhoza <ceddymuhoza@gmail.com>
2020-09-28 20:46:01 -0400

    fix contracts

--- src/contracts/contracts.test.ts
+++ src/contracts/contracts.test.ts
@@ -5,57 +5,39 @@ import { log } from '../log';
 import { IBKRConnection } from '../connection';
 
 const symbol = "ITCI"
-// const contractDetails = { "conId": 347408543, "symbol": "MAXR", "secType": "STK", "expiry": "", "strike": 0, "right": "", "multiplier": "", "exchange": "NYSE", "currency": "USD", "localSymbol": "MAXR", "tradingClass": "MAXR" }
-// const contractDetails = { "conId": 141253540, "symbol": "ITCI", "secType": "STK", "expiry": "", "strike": 0, "right": "", "multiplier": "", "exchange": "NASDAQ", "currency": "USD", "localSymbol": "ITCI", "tradingClass": "NMS" }
 
 before((done) => {
     ibkr().then(ran => done());
 });
 
 describe('Contracts', () => {
-    // it('should get contract details by symbol', (done) => {
-    //     async function getContractDetailsData() {
-    //         const contractDetails = await getContractDetails(symbol);
-    //         if (!Array.isArray(contractDetails)) {
-    //             log('contract ', JSON.stringify(contractDetails))
-    //             return done();
-    //         }
-    //         done(new Error('Error getting symbol details'));
-    //     }
-    //     getContractDetailsData();
-    // });
-
-    it('should get contract details by contract object', (done) => {
+    it('should get contract details by symbol', (done) => {
         async function getContractDetailsData() {
-            const ib = IBKRConnection.Instance.getIBKR();
+            const contractDetails = await getContractDetails(symbol);
+            if (!Array.isArray(contractDetails)) {
+                log('contract ', JSON.stringify(contractDetails))
+                return done();
+            }
+            done(new Error('Error getting symbol details'));
+        }
+        getContractDetailsData();
+    });
 
-            // @ts-ignore
-            // const contractOb = ib.contract.option('AAPL', '202012', 500, 'C');
+
+    it('should get contract details by options contract object', (done) => {
+        async function getContractDetailsData() {
 
             const contractObj = {
-                // "conId":12087792,
                 currency: 'USD',
                 exchange: 'SMART',
                 multiplier: 100,
                 right: 'C',
                 secType: 'OPT',
-                // strike: 300,
                 symbol: 'AAPL'
             }
 
             const contractDetails = await getContractDetails(contractObj);
 
-            /**
-             {
-                currency: 'USD',
-                exchange: 'SMART',
-                multiplier: 100,
-                right: 'C',
-                secType: 'OPT',
-                strike: 300,
-                symbol: 'AAPL'
-            }
-             */
             if (contractDetails) {
                 log('contract ', JSON.stringify(contractDetails))
                 return done();
@@ -65,5 +47,25 @@ describe('Contracts', () => {
         getContractDetailsData();
     });
 
+    it('should get contract details by forex contract object', (done) => {
+        async function getContractDetailsData() {
+
+            const contractObj = {
+                "symbol":"GBP",
+                "secType":"CASH",
+                "currency":"USD",
+                 // "localSymbol":"GBP.USD",
+            };
+
+            const contractDetails = await getContractDetails(contractObj);
+
+            if (contractDetails) {
+                log('contract ', JSON.stringify(contractDetails))
+                return done();
+            }
+            done(new Error('Error getting symbol details'));
+        }
+        getContractDetailsData();
+    });
 
 })
