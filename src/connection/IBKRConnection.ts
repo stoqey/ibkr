0f74f3cd5c241a857cbcfb839dcdd6dac54cb657
Ceddy Muhoza <ceddymuhoza@gmail.com>
2021-04-24 23:54:00 -0400

    use api next

--- src/connection/IBKRConnection.ts
+++ src/connection/IBKRConnection.ts
@@ -1,12 +1,12 @@
 import * as _ from 'lodash';
-import ibkr, {EventName as IBevents} from '@stoqey/ib';
+import {IBApiNext as ibkr} from '@stoqey/ib';
 import {IB_HOST, IB_PORT} from '../config';
 import {publishDataToTopic} from '../events/IbkrEvents.publisher';
 import {IBKREVENTS, IbkrEvents} from '../events';
 import {ConnectionStatus} from './connection.interfaces';
-import AccountSummary from '../account/AccountSummary';
-import {Portfolios} from '../portfolios';
-import Orders from '../orders/Orders';
+// import AccountSummary from '../account/AccountSummary';
+// import {Portfolios} from '../portfolios';
+// import Orders from '../orders/Orders';
 import includes from 'lodash/includes';
 import {log} from '../log';
 
@@ -39,7 +39,6 @@ export class IBKRConnection {
     public init = (host: string, port: number): void => {
         if (!this.ib) {
             this.ib = new ibkr({
-                clientId,
                 host,
                 port,
             });
@@ -56,20 +55,20 @@ export class IBKRConnection {
     public initialiseDep = async (): Promise<boolean> => {
         try {
             // 1. Account summary
-            log('1. Account summary');
-            const accountSummary = AccountSummary.Instance;
-            accountSummary.init();
-            await accountSummary.getAccountSummary();
-            // 2. Portfolios
-            log('2. Portfolios');
-            const portfolio = Portfolios.Instance;
-            await portfolio.init();
-            await portfolio.getPortfolios();
+            // log('1. Account summary');
+            // const accountSummary = AccountSummary.Instance;
+            // accountSummary.init();
+            // await accountSummary.getAccountSummary();
+            // // 2. Portfolios
+            // log('2. Portfolios');
+            // const portfolio = Portfolios.Instance;
+            // await portfolio.init();
+            // await portfolio.getPortfolios();
 
-            log('3. Orders');
-            const openOrders = Orders.Instance;
-            await openOrders.init();
-            await openOrders.getOpenOrders();
+            // log('3. Orders');
+            // const openOrders = Orders.Instance;
+            // await openOrders.init();
+            // await openOrders.getOpenOrders();
 
             return true;
         } catch (error) {
@@ -93,10 +92,9 @@ export class IBKRConnection {
             return log(IBKREVENTS.DISCONNECTED, `Error connecting client => ${clientId}`);
         }
 
-        // Important listners
-        this.ib.on(IBevents.connected, function () {
-            async function connectApp() {
-                log(`.................................................................`);
+        this.ib.connectionState.subscribe({
+            next: async (value: any) => {
+                log(`................................................................. ${value}`);
                 log(`...... Connected client ${clientId}, initialising services ......`);
 
                 // initialise dependencies
@@ -113,29 +111,17 @@ export class IBKRConnection {
                     log(`...... Successfully running ${clientId}'s services ..`);
                     return log(`.....................................................`);
                 }
+            },
+            error: (err: any) => {
+                const message = err && err.message;
 
-                disconnectApp();
-            }
-            connectApp();
-        });
+                log(IBKREVENTS.ERROR, err && err.message);
 
-        this.ib.on(IBevents.error, function (err: any) {
-            const message = err && err.message;
-
-            log(IBKREVENTS.ERROR, err && err.message);
-
-            if (includes(message, 'ECONNREFUSED') || (err && err.code === 'ECONNREFUSED')) {
-                return disconnectApp();
-            }
-        });
-
-        this.ib.on(IBevents.disconnected, function () {
-            log(IBKREVENTS.DISCONNECTED, `${clientId} Connection disconnected`);
-            disconnectApp();
-
-            // Process has to stop when IBKR is disconnected
-            // TODO reconnect process
-            process.exit(1);
+                if (includes(message, 'ECONNREFUSED') || (err && err.code === 'ECONNREFUSED')) {
+                    return disconnectApp();
+                }
+                process.exit(1);
+            },
         });
 
         // connect the IBKR
@@ -152,6 +138,10 @@ export class IBKRConnection {
                 });
             }
         });
+
+        setTimeout(() => {
+            console.log('done');
+        }, 5000);
     };
 
     /**
